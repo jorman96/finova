3b5ba5ec91e09e90fbc0c4e819de4e4b96d9c958
Jorma <jorma@finova.test>
2026-05-31 21:24:59 -0500

    Revert to env variables for Vercel

--- src/lib/firebase-admin.ts
+++ src/lib/firebase-admin.ts
@@ -1,11 +1,13 @@
 import * as admin from 'firebase-admin';
 
-import serviceAccount from './service-account.json';
-
 if (!admin.apps.length) {
   try {
     admin.initializeApp({
-      credential: admin.credential.cert(serviceAccount as any),
+      credential: admin.credential.cert({
+        projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+        clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+        privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+      }),
     });
   } catch (error) {
     console.error('Firebase admin initialization error', error);
